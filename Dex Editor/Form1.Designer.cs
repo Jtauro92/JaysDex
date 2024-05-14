79c8cc23e9672fe6b383444ae6db94700931d968
Jason Rush <jrush19@my.devry.edu>
2024-05-13 20:34:33 -0500

    Changes to be committed: 	deleted:    .vs/Pokedex/FileContentIndex/f62c9b58-85a7-47f1-b688-f393fa0b75ed.vsidx 	deleted:    .vs/Pokedex/v17/.wsuo 	deleted:    .vs/VSWorkspaceState.json 	deleted:    .vs/slnx.sqlite 	deleted:    .vscode/settings.json 	new file:   Dex Editor/.vs/Dex Editor/FileContentIndex/829f9011-1fc5-46ed-800f-49028596e6a7.vsidx 	new file:   Dex Editor/.vs/Dex Editor/FileContentIndex/9fdd556a-fbd8-4dc9-8a55-09b206a840ad.vsidx 	new file:   Dex Editor/.vs/Dex Editor/v17/.suo 	new file:   Dex Editor/App.config 	new file:   Dex Editor/Dex Editor.csproj 	new file:   Dex Editor/Dex Editor.sln 	new file:   Dex Editor/Form1.Designer.cs 	new file:   Dex Editor/Form1.cs 	new file:   Dex Editor/Form1.resx 	new file:   Dex Editor/Program.cs 	new file:   Dex Editor/Properties/AssemblyInfo.cs 	new file:   Dex Editor/Properties/Resources.Designer.cs 	new file:   Dex Editor/Properties/Resources.resx 	new file:   Dex Editor/Properties/Settings.Designer.cs 	new file:   Dex Editor/Properties/Settings.settings 	new file:   Dex Editor/bin/Debug/Dex Editor.exe 	new file:   Dex Editor/bin/Debug/Dex Editor.exe.config 	new file:   Dex Editor/bin/Debug/Dex Editor.pdb 	new file:   Dex Editor/obj/Debug/.NETFramework,Version=v4.8.AssemblyAttributes.cs 	new file:   Dex Editor/obj/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   Dex Editor/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   Dex Editor/obj/Debug/Dex Editor.csproj.AssemblyReference.cache 	new file:   Dex Editor/obj/Debug/Dex Editor.csproj.CoreCompileInputs.cache 	new file:   Dex Editor/obj/Debug/Dex Editor.csproj.FileListAbsolute.txt 	new file:   Dex Editor/obj/Debug/Dex Editor.csproj.GenerateResource.cache 	new file:   Dex Editor/obj/Debug/Dex Editor.exe 	new file:   Dex Editor/obj/Debug/Dex Editor.pdb 	new file:   Dex Editor/obj/Debug/Dex_Editor.Properties.Resources.resources 	new file:   Dex Editor/obj/Debug/Dex_Editor.mainForm.resources 	new file:   Pokedex.pyproj 	modified:   Pokemon.py 	modified:   __pycache__/Pokemon.cpython-311.pyc 	new file:   __pycache__/Pokemon.cpython-312.pyc

---
+++ Dex Editor/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Dex_Editor
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.outputbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nameMaskedTextBox
+            // 
+            this.nameMaskedTextBox.Location = new System.Drawing.Point(95, 53);
+            this.nameMaskedTextBox.Name = "nameMaskedTextBox";
+            this.nameMaskedTextBox.Size = new System.Drawing.Size(192, 26);
+            this.nameMaskedTextBox.TabIndex = 1;
+            // 
+            // outputBox
+            // 
+            this.outputBox.BackColor = System.Drawing.SystemColors.Info;
+            this.outputBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.outputBox.Location = new System.Drawing.Point(342, 41);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(320, 562);
+            this.outputBox.TabIndex = 2;
+            this.outputBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // outputbutton
+            // 
+            this.outputbutton.Location = new System.Drawing.Point(95, 146);
+            this.outputbutton.Name = "outputbutton";
+            this.outputbutton.Size = new System.Drawing.Size(171, 103);
+            this.outputbutton.TabIndex = 3;
+            this.outputbutton.Text = "Show";
+            this.outputbutton.UseVisualStyleBackColor = true;
+            this.outputbutton.Click += new System.EventHandler(this.outputbutton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(686, 629);
+            this.Controls.Add(this.outputbutton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.nameMaskedTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "mainForm";
+            this.Text = "Dex Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox nameMaskedTextBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button outputbutton;
+    }
+}
+
