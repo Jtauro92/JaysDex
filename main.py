1406d89854237c3ff0d0135166643e5986b6343f
Jason Rush <134960786+Jtauro92@users.noreply.github.com>
2024-06-24 19:12:29 -0500

    Refactor main.py to import necessary modules and improve code organization

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from multiprocessing import process
 from add_pokemon import Add_Pokemon as AP
 from add_stats import add_stats as AS
 from view_pokemon import View_Pokemon as VP
@@ -6,23 +5,23 @@ from menu import Menu as M
 import msvcrt as MS
 from rich.console import Console 
 from rich import print as rprint
-console = Console()
+
+clear_console = Console().clear
+def minput():
+        MS.kbhit()
+        return MS.getch()
 
 class dex_menu:
     def __init__(self):
-        self.ap = AP().main
-        self.as_ = AS().main
-        self.vp = VP().main
-        self.joblist = [(b'1',self.ap),
-                        (b'2',self.as_),
-                        (b'3',self.vp)]
-        
+        self.joblist = [(b'1',AP().main),
+                        (b'2',AS().main),
+                        (b'3',VP().main)]  
+    
     def set_option(self,number= None,version=None):
         while (number not in [b'0',b'1',b'2',b'3']) or (int(number) == version):
-            rprint('\n[bold red]Invalid Entry. Try Again![/bold red]')
-            MS.kbhit()
+            rprint('\n[bold red]\nInvalid Entry. Try Again![/bold red]')
+            number = minput()
             print("\033[A\033[2K\033[A", end='')
-            number = MS.getch()
         return number
         
     def processes(self,hit):
@@ -34,16 +33,13 @@ class dex_menu:
 
     def main(self):
         M().main_menu()
-        MS.kbhit()
-        selection = self.set_option(MS.getch())
+        selection = self.set_option(minput())
         while selection != b'0':
-            version = self.processes(selection)
+            process_num = self.processes(selection)
             M().main_menu(int(selection))
-            MS.kbhit()
-            selection = self.set_option(MS.getch(),version)
-        console.clear()
+            selection = self.set_option(minput(),process_num)
+        clear_console()
         rprint('[bold green]Goodbye![/bold green]')
   
 if __name__ == '__main__':
-
     dex_menu().main()
