70ac1b34811021c320b6f8ec776e3f2717fabf23
Jason Rush <134960786+Jtauro92@users.noreply.github.com>
2024-12-17 06:32:58 -0600

    Refactor Pokemon class for improved attribute handling, validation, and input processing

--- Pokemon.py
+++ Pokemon.py
@@ -14,16 +14,16 @@ def minput():
         return m.getch()
 
 class Pokemon:
-    def __init__(self,name,number=0,type='',type2=None,ability=None,ability2=None,h_ability=None,
+    def __init__(self,name='',number=None,type='',type2='',ability='',ability2=None,h_ability=None,
                  stage=1,hp=0,atk=0,defn=0,sp_atk=0,sp_def=0,speed=0):
         
-        self.name = name if self.validate_pokemon(name) else None
-        self.number = number if self.validate_pokemon(number) else None
-        self.type1 = type.upper() if type.upper() in type_list or type != ''.strip() else None
-        self.type2 = type2
-        self.ability = ability if self.validate_ability(ability) else None
-        self.ability2 = ability2
-        self.h_ability =  ability2
+        self.name = name.title() if self.validate_pokemon(name) else None
+        self.number = int(number) if self.validate_pokemon(number=number) else None
+        self.type1 = type.upper() if self.validate_pokemon(type=type) else None
+        self.type2 = type2.upper() if self.validate_pokemon(type2=type2) else None
+        self.ability = ability.title() if self.validate_pokemon(ability=ability) else None
+        self.ability2 = ability2.titlte() if self.validate_pokemon(ability2=ability2) else None
+        self.h_ability =  h_ability.title() if self.validate_pokemon(h_ability=h_ability) else None
         self.stage = stage if 0 < stage < 4 else 1
         
         self.hp = hp
@@ -34,23 +34,30 @@ class Pokemon:
         self.speed = speed
         self.in_pokedex = False
         
-    def validate_pokemon(self,name=None,number=0):
-        if any (pokemon['Name'] == name for pokemon in dex):
-            yield True
-        if any (pokemon['Number'] == number for pokemon in dex):
+    def validate_pokemon(self, name=None, number=None, type=None, type2=None, ability=None,
+                         ability2=None, h_ability=None):
+        if name and any(pokemon['Name'] == name.title() for pokemon in dex):
+            return True
+        
+        elif number and any(pokemon['Number'] == number for pokemon in dex):
+            return True
+        
+        if type and type.upper() in type_list:
+            return True
+
+        elif type2 and self.validate_pokemon(type=type2) and type2.upper() != self.type1:
+            return True
+        
+        if ability and ability.title() in ability_list:
+            return True
+        
+        elif ability2 and self.validate_pokemon(ability=ability2) and ability2.title() != self.ability:
+            return True
+        
+        elif h_ability and self.validate_pokemon(ability=h_ability) and h_ability.title() not in [self.ability,self.ability2]:
             return True
         
         return False
-
-     
-        
-    def validate_ability(self,ability=None):
-        if ability in ability_list:
-            return True
-        else:
-            return False
-   
-
 
     def in_pokedex(self,name=None,number=None):
         if isinstance(name,str):
@@ -58,13 +65,6 @@ class Pokemon:
         elif isinstance(number,int):
             return any(pokemon['Number'] == number for pokemon in dex)
             
-    def type2_equals_type1(self):
-        return self.type1 == self.type2
-
-    def has_same_ability(self):
-        if (self.ability2 is not None) and (self.ability2 == self.ability):
-            return True 
-        return False   
         
     def __str__(self):
         return f"pokemon: {self.name}\n\
@@ -83,10 +83,9 @@ sp_def: {self.sp_def}\n\
 speed: {self.speed}\n"             
 
 if __name__ == '__main__':
-    Testmon = Pokemon(name="Charizard",
-                      number=8000, 
-                      type="Fire",
-                      type2="Flying",
+    Testmon = Pokemon(number=25,
+                      type="flying",
+                      type2="fire",
                       ability="drought",
                       ability2="drought",
                       h_ability="",)
